82b7c186e0196369f5f96d52a1bc994a2dcbac0d
Carlos Rodriguez <carlos@s8f.org>
2012-06-12 16:12:12 -0700

    Expand docs

--- test/model.js
+++ test/model.js
@@ -56,7 +56,6 @@ describe('model/collection', function() {
   });
 
   it('can destroy the model', function(done) {
-    /*
     myModel.destroy(function(err) {
       assert.ifError(err);
       coll.get(myId, function(err, model) {
@@ -65,8 +64,6 @@ describe('model/collection', function() {
         done();
       });
     });
-     */
-    done();
   });
 
   it('can run before/after hooks', function(done) {
