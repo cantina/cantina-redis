060574a09678bf1ccfe20e2ee9ea60c7621d5afc
Carlos Rodriguez <carlos@s8f.org>
2012-06-20 11:22:51 -0700

    Update example

--- examples/example.js
+++ examples/example.js
@@ -1,25 +1,30 @@
-var cantina = require('cantina')
-  , RedisCollection = require('../').RedisCollection;
+var utils = require('cantina-utils')
+  , RedisCollection = require('../').RedisCollection
+  , RedisModel = require('../').RedisModel
+  ;
 
-var app = cantina.createApp();
-app.use(require('../').plugin);
-app.start(function() {
-  var schema = {
+function Fruit(attrs, options) {
+  Fruit.super.call(this, attrs, options);
+}
+utils.inherits(Fruit, RedisModel);
+Fruit.schema = {
+  name: 'fruit',
+  properties: {
     type: {
       type: 'string',
       index: true
     },
     timestamp: {
       type: 'object',
-      default: new Date()
+      default: function() { return new Date(); }
     }
-  };
-  var fruit = new RedisCollection({namespace: 'fruit', schema: schema, client: app.redis});
-  fruit.create({type: 'apple', color: 'red'}, function(err, apple) {
-    console.log(apple.id, 'id');
-    console.log(apple.validate(), 'validate()');
-    fruit.find({type: 'apple'}, {sort: 'timestamp', desc: true, limit: 5}, function(err, apples) {
-      console.log(apples[0].properties, 'model found');
-    });
+  }
+};
+var fruit = new RedisCollection({model: Fruit, client: require('../').createClient()});
+fruit.create({type: 'apple', color: 'red'}, function(err, apple) {
+  console.log(apple.id, 'id');
+  console.log(apple.validate(), 'validate()');
+  fruit.find({type: 'apple'}, {sort: 'timestamp', desc: true, limit: 5}, function(err, apples) {
+    console.log(apples[0].properties, 'model found');
   });
 });
