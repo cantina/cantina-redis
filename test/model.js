d8cac3bfa1981a59a0371227b65a3bb38c3dc750
Carlos Rodriguez <carlos@s8f.org>
2012-06-20 11:18:16 -0700

    Redo schemas to be static property

--- test/model.js
+++ test/model.js
@@ -9,15 +9,41 @@ var utils = require('cantina-utils')
   ;
 
 describe('model/collection', function() {
-  var coll, cleanup = [];
+  var coll, cleanup = [], MyModel;
+  function assertModel(model) {
+    assert.ok(model, 'model is ok');
+    cleanup.push(model);
+  }
+  function assertValid(err) {
+    if (utils.isArray(err)) {
+      console.error(err);
+      assert.fail('validation errors');
+    }
+    else {
+      assert.ifError(err);
+    }
+  }
   before(function() {
-    var schema = {
-      job: {
-        type: 'string',
-        index: true
+    MyModel = function MyModel(attrs, options) {
+      MyModel.super.call(this, attrs, options);
+    }
+    utils.inherits(MyModel, RedisModel);
+    MyModel.schema = {
+      properties: {
+        job: {
+          type: 'string',
+          index: true
+        },
+        color: {
+          type: 'string',
+          index: true
+        },
+        timestamp: {
+          type: 'number'
+        }
       }
     };
-    coll = new RedisCollection({client: redis.createClient(), schema: schema});
+    coll = new RedisCollection({client: redis.createClient(), model: MyModel});
   });
 
   after(function() {
@@ -25,13 +51,14 @@ describe('model/collection', function() {
     while (model = cleanup.pop()) {
       model.destroy();
     }
+    assert.strictEqual(cleanup.length, 0, 'no models left over');
   });
 
   var myId;
   it('can create a model', function(done) {
-    var model = new RedisModel({name: 'carlos'}, coll);
+    var model = new MyModel({name: 'carlos'}, coll);
     model.save(function(err) {
-      assert.ifError(err);
+      assertValid(err);
       myId = model.id;
       assert.ok(myId);
       done(err);
@@ -41,7 +68,7 @@ describe('model/collection', function() {
   var myModel;
   it('can get the model', function(done) {
     coll.get(myId, function(err, model) {
-      assert.ifError(err);
+      assertValid(err);
       assert.ok(model, 'got a model back');
       assert.strictEqual(model.id, myId);
       assert.strictEqual(model.properties.name, 'carlos');
@@ -51,7 +78,7 @@ describe('model/collection', function() {
 
   it('can update the model', function(done) {
     coll.update(myId, {name: 'scooby'}, function(err, model) {
-      assert.ifError(err);
+      assertValid(err);
       assert.ok(model, 'got a model back');
       assert.strictEqual(model.id, myId);
       assert.strictEqual(model.properties.name, 'scooby');
@@ -72,13 +99,13 @@ describe('model/collection', function() {
   });
 
   it('can run before/after hooks', function(done) {
-    var model = new RedisModel({name: 'buster'}, coll);
-    cleanup.push(model);
+    var model = new MyModel({name: 'buster'}, coll);
+    assertModel(model);
     model.on('save:before', function(model) {
       this.properties.job = 'dog';
     });
     model.save(function(err) {
-      assert.ifError(err);
+      assertValid(err);
       coll.get(model.id, function(err, saved) {
         assert.ifError(err);
         assert.strictEqual(saved.properties.job, 'dog', 'property from hook was saved');
@@ -89,11 +116,11 @@ describe('model/collection', function() {
 
   it('can query an index', function(done) {
     coll.create({name: 'egon', job: 'ghostbuster'}, function(err, model) {
-      assert.ifError(err);
-      cleanup.push(model);
+      assertValid(err);
+      assertModel(model);
       coll.create({name: 'kobe', job: 'baller'}, function(err, model) {
-        assert.ifError(err);
-        cleanup.push(model);
+        assertValid(err);
+        assertModel(model);
         coll.find({job: 'ghostbuster'}, function(err, models) {
           assert.ifError(err);
           assert.strictEqual(models.length, 1);
@@ -106,11 +133,11 @@ describe('model/collection', function() {
 
   it('can findOne with an indexed query', function(done) {
     coll.create({name: 'egon', job: 'ghostbuster'}, function(err, model) {
-      assert.ifError(err);
-      cleanup.push(model);
+      assertValid(err);
+      assertModel(model);
       coll.create({name: 'kobe', job: 'baller'}, function(err, model) {
-        assert.ifError(err);
-        cleanup.push(model);
+        assertValid(err);
+        assertModel(model);
         coll.findOne({job: 'ghostbuster'}, function(err, model) {
           assert.ifError(err);
           assert.strictEqual(model.properties.name, 'egon');
@@ -122,21 +149,18 @@ describe('model/collection', function() {
 
   it('can sort and limit', function(done) {
     var tasks = [], max = 0;
-    coll.schema.timestamp = {
-      type: 'number'
-    };
     for (var i = 0; i < 100; i++) {
       tasks.push(function(cb) {
         var rand = Math.random();
         max = Math.max(rand, max);
         coll.create({timestamp: rand}, function(err, model) {
-          cleanup.push(model);
+          assertModel(model);
           cb(err, model);
         });
       })
     }
     utils.async.parallel(tasks, function(err, results) {
-      assert.ifError(err);
+      assertValid(err);
       assert.ok(results, 'created some timestamp records');
       coll.find({}, {sort: 'timestamp', desc: true, limit: 70, skip: 1}, function(err, models) {
         assert.ifError(err);
@@ -156,7 +180,6 @@ describe('model/collection', function() {
 
   it('can filter with sort and skip/limit', function(done) {
     var tasks = [], blue_min = 1;
-    coll.indexes.push('color');
     coll.schema.timestamp = {
       type: 'number'
     };
@@ -164,7 +187,7 @@ describe('model/collection', function() {
       tasks.push(function(cb) {
         var rand = Math.random();
         coll.create({color: 'red', timestamp: rand}, function(err, model) {
-          cleanup.push(model);
+          assertModel(model);
           cb(err, model);
         });
       })
@@ -174,13 +197,13 @@ describe('model/collection', function() {
         var rand = Math.random();
         blue_min = Math.min(rand, blue_min);
         coll.create({color: 'blue', timestamp: rand}, function(err, model) {
-          cleanup.push(model);
+          assertModel(model);
           cb(err, model);
         });
       })
     }
     utils.async.parallel(tasks, function(err, results) {
-      assert.ifError(err);
+      assertValid(err);
       assert.ok(results, 'created some records');
       coll.find({color: 'blue'}, {sort: 'timestamp', limit: 10, skip: 1}, function(err, models) {
         assert.ifError(err);
@@ -214,8 +237,8 @@ describe('model/collection', function() {
       patterns: [/blah/, /etc/]
     };
     coll.create(obj, function(err, model) {
-      cleanup.push(model);
-      assert.ifError(err);
+      assertValid(err);
+      assertModel(model);
       assert.deepEqual(model.toJSON(), obj);
       done();
     });
@@ -224,16 +247,23 @@ describe('model/collection', function() {
   describe('separate collection', function() {
     var fruit;
     before(function() {
-      var schema = {
-        color: {
-          type: 'number',
-          index: true
-        },
-        type: {
-          type: 'string'
+      function Fruit(attrs, options) {
+        Fruit.super.call(this, attrs, options);
+      }
+      utils.inherits(Fruit, RedisModel);
+      Fruit.schema = {
+        name: 'fruit',
+        properties: {
+          color: {
+            type: 'number',
+            index: true
+          },
+          type: {
+            type: 'string'
+          }
         }
       };
-      fruit = new RedisCollection({namespace: 'fruit', schema: schema, client: redis.createClient()});
+      fruit = new RedisCollection({model: Fruit, client: redis.createClient()});
     });
 
     it('can create a new collection', function(done) {
@@ -246,18 +276,18 @@ describe('model/collection', function() {
 
     it('can add a model to new collection', function(done) {
       fruit.create({type: 'banana', color: 0xE3CF57}, function(err, model) {
-        assert.ifError(err);
+        assertValid(err);
         assert.strictEqual(model.properties.color, 0xE3CF57);
-        cleanup.push(model);
+        assertModel(model);
         done();
       });
     });
 
     it('can add a second model to new collection', function(done) {
       fruit.create({type: 'orange', color: 0xFF7D40}, function(err, model) {
-        assert.ifError(err);
+        assertValid(err);
         assert.strictEqual(model.properties.color, 0xFF7D40);
-        cleanup.push(model);
+        assertModel(model);
         done();
       });
     });
